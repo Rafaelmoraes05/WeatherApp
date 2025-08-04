f15ef70db444a1ee55b45e87149cb95452744a07
Rafael Moraes <rafaelmoraes005@gmail.com>
2025-08-04 19:12:35 -0300

    [feat]: Prática 08 – Parte 1 - Mostrando as condições climáticas atuais na lista de cidades

--- app/src/main/java/com/weatherapp/ui/ListPage.kt
+++ app/src/main/java/com/weatherapp/ui/ListPage.kt
@@ -32,6 +32,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.weatherapp.model.City
 import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.LaunchedEffect
 import com.weatherapp.viewModel.MainViewModel
 
 
@@ -48,9 +49,16 @@ fun ListPage(
             .padding(8.dp)
     ) {
         items(cityList, key = { it.name }) { city ->
+            LaunchedEffect(city.name) {
+                if (city.weather == null) {
+                    viewModel.loadWeather(city.name)
+                }
+            }
+
             CityItem(city = city, onClose = {
                 viewModel.remove(city)
-            }, onClick = {
+            },
+                onClick = {
                 android.widget.Toast.makeText(
                     activity,
                     "Clicou em ${city.name}",
@@ -82,7 +90,7 @@ fun CityItem(
                 text = city.name,
                 fontSize = 24.sp)
             Text(modifier = Modifier,
-                text = city.weather?:"Carregando clima...",
+                text = city.weather?.desc?:"carregando...",
                 fontSize = 16.sp)
         }
         IconButton(onClick = onClose) {
--- app/src/main/java/com/weatherapp/viewModel/MainViewModel.kt
+++ app/src/main/java/com/weatherapp/viewModel/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.weatherapp.viewModel
 
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.weatherapp.model.City
@@ -8,6 +9,7 @@ import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.maps.model.LatLng
 import com.weatherapp.api.WeatherService
+import com.weatherapp.api.toWeather
 import com.weatherapp.db.fb.FBCity
 import com.weatherapp.db.fb.FBDatabase
 import com.weatherapp.db.fb.FBUser
@@ -16,9 +18,10 @@ import com.weatherapp.model.User
 
 class MainViewModel (private val db: FBDatabase,
                      private val service : WeatherService): ViewModel(), FBDatabase.Listener {
-    private val _cities = mutableStateListOf<City>()
-    val cities
-        get() = _cities.toList()
+    private val _cities = mutableStateMapOf<String, City>()
+    val cities : List<City>
+        get() = _cities.values.toList()
+
     private val _user = mutableStateOf<User?> (null)
     val user : User?
         get() = _user.value
@@ -54,13 +57,22 @@ class MainViewModel (private val db: FBDatabase,
         //TODO("Not yet implemented")
     }
     override fun onCityAdded(city: FBCity) {
-        _cities.add(city.toCity())
+        _cities[city.name!!] = city.toCity()
     }
     override fun onCityUpdated(city: FBCity) {
-        //TODO("Not yet implemented")
+        _cities.remove(city.name)
+        _cities[city.name!!] = city.toCity()
     }
     override fun onCityRemoved(city: FBCity) {
-        _cities.remove(city.toCity())
+        _cities.remove(city.name)
+    }
+
+    fun loadWeather(name: String) {
+        service.getWeather(name) { apiWeather ->
+            val newCity = _cities[name]!!.copy( weather = apiWeather?.toWeather() )
+            _cities.remove(name)
+            _cities[name] = newCity
+        }
     }
 }
 
