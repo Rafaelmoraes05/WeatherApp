d78738c93c7e6ef273ba0ccc357dea2fdfb8a7df
Rafael Moraes <rafaelmoraes005@gmail.com>
2025-09-04 00:34:28 -0300

    [feat]: Prática 11 – Parte 1 : Configuração das dependências do Room.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,24 +1,23 @@
 import java.util.Properties
 
 plugins {
-
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
-
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
     id ("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
     alias(libs.plugins.google.gms.google.services)
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2"
 }
 
 android {
     namespace = "com.weatherapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.weatherapp"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -31,6 +30,11 @@ android {
             props.getProperty("WEATHER_API_KEY"))
 
     }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
+
 
     buildTypes {
         release {
@@ -55,6 +59,11 @@ android {
 }
 
 dependencies {
+
+    val room_version = "2.7.2"
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
 
     implementation("androidx.work:work-runtime-ktx:2.9.0")
     // coil
--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,10 @@ buildscript {
 
 plugins {
 
-    kotlin("plugin.serialization") version "1.9.0" apply false
-
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    kotlin("plugin.serialization") version "1.9.0" apply false
     alias(libs.plugins.google.gms.google.services) apply false
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
 }
